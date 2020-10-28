7986e71eef873f0b5ae004cdb82719914d60641a
darrell bazian <darrellbazianjr@gmail.com>
2020-10-28 09:42:34 -0400

    fixed permissions will ask again if denied and if denied twice will send to settings on android

--- src/components/AudioPlayer/Download.android.js
+++ src/components/AudioPlayer/Download.android.js
@@ -4,83 +4,82 @@ import {
   Platform,
   PermissionsAndroid,
   Alert,
+  Linking,
 } from "react-native";
 import { heightPercentageToDP as hp } from "react-native-responsive-screen";
-import { useSelector } from "react-redux";
 import RNFetchBlob from "rn-fetch-blob";
 import storage from "@react-native-firebase/storage";
-
 import SmallIndicator from "../Indicators/SmallIndicator";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faDownload } from "@fortawesome/pro-light-svg-icons";
 import Colors from "../../../constants/Colors";
 
 const Download = (props) => {
-  const isAuth = useSelector((state) => !!state.auth.token);
   const [isLoading, setIsLoading] = useState(false);
-
-  let audioFile = props.audioFile;
-  const reference = storage().ref(audioFile);
-  let task = reference.getDownloadURL();
+  const task = storage().ref(props.audioFile).getDownloadURL();
 
   const downloadPermission = async () => {
     try {
       const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
-        {
-          title: "Storage Permission",
-          message: "App needs access to memory to download the file ",
-        }
+        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE
       );
+
       if (granted === PermissionsAndroid.RESULTS.GRANTED) {
         console.log("permissions granted");
-      } else {
-        console.log("permissions denied");
+        const source = await task;
+        const {
+          dirs: { DownloadDir },
+        } = RNFetchBlob.fs;
+        const { config } = RNFetchBlob;
+        const aPath = DownloadDir;
+        var ext = "mp3";
+        var file_ex = props.audioFile;
+        const configOptions = Platform.select({
+          android: {
+            fileCache: false,
+            appendExt: ext,
+            addAndroidDownloads: {
+              useDownloadManager: true,
+              notification: true,
+              path: aPath + "/" + file_ex,
+              description: "Song",
+            },
+          },
+        });
+        setIsLoading(true);
+        config(configOptions)
+          .fetch("GET", source)
+          .then((res) => {
+            console.log("file_download", res);
+            setIsLoading(false);
+            Alert.alert("Your file is done downloading!");
+          })
+          .catch((errorMessage) => {
+            setIsLoading(false);
+            console.log("error with downloading file", errorMessage);
+          });
+      } else if (granted === PermissionsAndroid.RESULTS.DENIED) {
+        console.log("permission denied");
+      } else if (granted === PermissionsAndroid.RESULTS.NEVER_ASK_AGAIN) {
+        Alert.alert(
+          "In order to download this file you will need to accept permissions in your settings",
+          "Do you wish to continue?",
+          [
+            {
+              text: "Ok",
+              onPress: () => Linking.openSettings(),
+            },
+            {
+              text: "Cancel",
+              onPress: () => console.log("cancelled"),
+              style: "cancel",
+            },
+          ],
+          { cancelable: false }
+        );
       }
     } catch (err) {
       console.warn(err);
-    }
-  };
-  downloadPermission();
-
-  const downloadSong = async () => {
-    Platform.select({
-      android: downloadPermission(),
-    });
-
-    if (isAuth) {
-      const source = await task;
-      const {
-        dirs: { DownloadDir },
-      } = RNFetchBlob.fs;
-      const { config } = RNFetchBlob;
-      const aPath = DownloadDir;
-      var ext = "mp3";
-      var file_ex = props.audioFile;
-      const configOptions = Platform.select({
-        android: {
-          fileCache: false,
-          appendExt: ext,
-          addAndroidDownloads: {
-            useDownloadManager: true,
-            notification: true,
-            path: aPath + "/" + file_ex,
-            description: "Song",
-          },
-        },
-      });
-      setIsLoading(true);
-      config(configOptions)
-        .fetch("GET", source)
-        .then((res) => {
-          console.log("file_download", res);
-          setIsLoading(false);
-          Alert.alert("Your file is done downloading!");
-        })
-        .catch((errorMessage) => {
-          setIsLoading(false);
-          console.log("error with downloading file", errorMessage);
-        });
     }
   };
 
@@ -88,7 +87,7 @@ const Download = (props) => {
     return <SmallIndicator />;
   }
   return (
-    <TouchableOpacity onPress={downloadSong}>
+    <TouchableOpacity onPress={downloadPermission}>
       <FontAwesomeIcon
         icon={faDownload}
         size={hp("5%")}
--- src/components/AudioPlayer/Download.ios.js
+++ src/components/AudioPlayer/Download.ios.js
@@ -1,22 +1,18 @@
 import React, { useState } from "react";
-import { TouchableOpacity } from "react-native";
+import { TouchableOpacity, Alert } from "react-native";
 import RNFetchBlob from "rn-fetch-blob";
 import storage from "@react-native-firebase/storage";
 import { heightPercentageToDP as hp } from "react-native-responsive-screen";
-
 import SmallIndicator from "../Indicators/SmallIndicator";
-
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faDownload } from "@fortawesome/pro-light-svg-icons";
 import Colors from "../../../constants/Colors";
 
 const Download = (props) => {
   const [isLoading, setIsLoading] = useState(false);
-  const reference = storage().ref(props.audioFile);
-  let task = reference.getDownloadURL();
 
   const downloadSong = async () => {
-    const source = await task;
+    const source = await storage().ref(props.audioFile).getDownloadURL();
     const {
       dirs: { DocumentDir },
     } = RNFetchBlob.fs;
@@ -36,6 +32,10 @@ const Download = (props) => {
       })
       .catch((e) => {
         console.log(e);
+        Alert.alert(
+          "There was a problem downloading your file please try again later."
+        );
+        setIsLoading(false);
       });
     return;
   };
