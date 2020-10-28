9770e69dea48eaf9f1aaa3139be3b6e4286a0f9b
darrell bazian <darrellbazianjr@gmail.com>
2020-10-28 13:02:16 -0400

    cleaned up wrappers

--- src/components/Wrappers/Card.js
+++ src/components/Wrappers/Card.js
@@ -1,10 +1,7 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
 import Colors from "../../../constants/Colors";
-import {
-  widthPercentageToDP as wp,
-  heightPercentageToDP as hp,
-} from "react-native-responsive-screen";
+import { widthPercentageToDP as wp } from "react-native-responsive-screen";
 
 const Card = (props) => {
   return <View style={styles.container}>{props.children}</View>;
