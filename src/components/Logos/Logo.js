7df65ab54eede0e59b4305ebef791d800078311c
darrell bazian <darrellbazianjr@gmail.com>
2020-10-29 13:35:33 -0400

    adjusted images

--- src/components/Logos/Logo.js
+++ src/components/Logos/Logo.js
@@ -1,17 +1,11 @@
 import React from "react";
 import { View, Image, StyleSheet } from "react-native";
-import {
-  widthPercentageToDP as wp,
-  heightPercentageToDP as hp,
-} from "react-native-responsive-screen";
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from "react-native-responsive-screen";
 
 const Logo = () => {
   return (
-    <View style={styles.imageContainer}>
-      <Image
-        source={require("../../../assets/logo-image.png")}
-        style={styles.image}
-      />
+    <View>
+      <Image source={require("../../../assets/logo-image.png")} style={styles.image} />
     </View>
   );
 };
--- src/components/Logos/LogoText.js
+++ src/components/Logos/LogoText.js
@@ -1,17 +1,11 @@
 import React from "react";
 import { View, Image, StyleSheet } from "react-native";
-import {
-  widthPercentageToDP as wp,
-  heightPercentageToDP as hp,
-} from "react-native-responsive-screen";
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from "react-native-responsive-screen";
 
 const LogoText = () => {
   return (
-    <View style={styles.logoContainer}>
-      <Image
-        source={require("../../../assets/logo-text.png")}
-        style={styles.logoText}
-      />
+    <View>
+      <Image source={require("../../../assets/logo-text.png")} style={styles.logoText} />
     </View>
   );
 };
@@ -19,11 +13,8 @@ const LogoText = () => {
 const styles = StyleSheet.create({
   logoText: {
     resizeMode: "contain",
-    width: wp("100%"),
+    width: wp("85%"),
     height: hp("15%"),
-  },
-  logoContainer: {
-    width: "100%",
   },
 });
 
